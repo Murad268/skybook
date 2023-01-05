a2501c42a3f637c42ee4820bde5cd447600993ba
Murad268 <82538242+Murad268@users.noreply.github.com>
2023-01-05 13:26:29 +0400

    ss

--- src/pages/friendsPage.php
+++ src/pages/friendsPage.php
@@ -1,10 +1,31 @@
 <?php
   include('./src/components/Header/header.php');
   include("./src/components/AsideLeft/AsideLeft.php");
-  $friendsSorgu = $dbh->prepare("SELECT * FROM friends WHERE friend_first = ? OR friend_second = ?");
+   if(isset($_REQUEST["sayfalama"])) {
+      $sayfalama = $_REQUEST["sayfalama"];
+   } else {
+      $sayfalama = 1;
+   }
+   if(isset($_REQUEST["page"])) {
+      $page = $_REQUEST["page"];
+      $sayfalamaKosulu = "&page=$page";
+   } else {
+      $sayfalamaKosulu = "";
+   }
+
+  $sayfalamaIcinButonSayisi = 2;
+  $sayfaBasinaGosterilecek = 10;
+  $toplamKayitSayisiSorgusu = $dbh->prepare("SELECT * FROM friends WHERE friend_first = ? OR friend_second = ?");
+  $toplamKayitSayisiSorgusu->execute([$user_id, $user_id]);
+  $toplamKayitSayisi = $toplamKayitSayisiSorgusu->rowCount();
+  $sayfalamayBaslayacaqKayotSayisi = ($sayfalama*$sayfaBasinaGosterilecek) - $sayfaBasinaGosterilecek;
+  $bulunanSafyaSayisi = ceil($toplamKayitSayisi/$sayfaBasinaGosterilecek);
+
+
+  $friendsSorgu = $dbh->prepare("SELECT * FROM friends WHERE friend_first = ? OR friend_second = ? LIMIT $sayfalamayBaslayacaqKayotSayisi, $sayfaBasinaGosterilecek");
   $friendsSorgu->execute([$user_id, $user_id]);
   $friends = $friendsSorgu->fetchAll(PDO::FETCH_ASSOC);
-  
+
 ?>
   <div class="friends__main">
    <div></div>
@@ -47,6 +68,34 @@
                </div>
             </div>
          </div>
+         <nav aria-label="Page navigation example">
+         <?php
+               if($bulunanSafyaSayisi>1) {?>
+                  <div class="paginationWrapper">
+                     <nav aria-label="Page navigation example ">
+                        <ul class="pagination">
+                        <li class="page-item"><a class="page-link" href="?sayfalama=1<? echo $sayfalamaKosulu?>">&laquo;</a></li>
+                        <?php
+                           for($i = $sayfalama-$sayfalamaIcinButonSayisi; $i <= $sayfalama+$sayfalamaIcinButonSayisi; $i++) {
+                              if(($i > 0) and ($i <= $bulunanSafyaSayisi)) {
+                                 $curr = $i;
+                              if($sayfalama == $i) {
+                                 echo "<li style=\"cursor: pointer\" class=\"page-item\"><div style=\"background: red; color: white\" class=\"page-link\">$curr</div></li>";
+                              } else {
+                                 echo "<li class=\"page-item\"><a class=\"page-link\" href=\"?sayfalama=$curr$sayfalamaKosulu\">$curr</a></li>";
+                              }
+                           }
+                        }
+                        ?>
+                           
+                           <li class="page-item"><a class="page-link"  href="?sayfalama=<?=$bulunanSafyaSayisi.$sayfalamaKosulu?>">&raquo;</a></li>
+                        </ul>
+                     </nav>
+                  </div>
+               <?php
+               }
+            ?>
+      </nav>
             <?php
          } else {?>
             <div class="no__friend">
@@ -58,6 +107,7 @@
          <?php
 
          }
+         
    ?>
    <div> </div>
 <?php
--- src/pages/messagessPage.php
+++ src/pages/messagessPage.php
@@ -1,7 +1,29 @@
 <?php
   include('./src/components/Header/header.php');
   include("./src/components/AsideLeft/AsideLeft.php");
-  $fethMessages = $dbh->prepare("SELECT * FROM messaggess WHERE (to_id=? AND from_id=?) OR (from_id=? AND to_id=?)");
+
+   if(isset($_REQUEST["sayfalama"])) {
+      $sayfalama = $_REQUEST["sayfalama"];
+   } else {
+      $sayfalama = 1;
+   }
+   if(isset($_REQUEST["page"])) {
+      $page = $_REQUEST["page"];
+      $mess = $_REQUEST["message"];
+      $sayfalamaKosulu = "&page=".$page."&message=".$mess;
+   } else {
+      $sayfalamaKosulu = "";
+   }
+
+   $sayfalamaIcinButonSayisi = 2;
+   $sayfaBasinaGosterilecek = 20;
+   $toplamKayitSayisiSorgusu = $dbh->prepare("SELECT * FROM messaggess WHERE (to_id=? AND from_id=?) OR (from_id=? AND to_id=?)");
+   $toplamKayitSayisiSorgusu->execute([$user_id, $_GET["message"], $user_id, $_GET["message"]]);
+   $toplamKayitSayisi = $toplamKayitSayisiSorgusu->rowCount();
+   $sayfalamayBaslayacaqKayotSayisi = ($sayfalama*$sayfaBasinaGosterilecek) - $sayfaBasinaGosterilecek;
+   $bulunanSafyaSayisi = ceil($toplamKayitSayisi/$sayfaBasinaGosterilecek);
+
+  $fethMessages = $dbh->prepare("SELECT * FROM messaggess WHERE (to_id=? AND from_id=?) OR (from_id=? AND to_id=?) LIMIT $sayfalamayBaslayacaqKayotSayisi, $sayfaBasinaGosterilecek");
   $fethMessages->execute([$user_id, $_GET["message"], $user_id, $_GET["message"]]);
   $messages = $fethMessages->fetchAll(PDO::FETCH_ASSOC);
   $getNemeFetch = $dbh->prepare("SELECT * FROM users WHERE id = ?");
@@ -40,7 +62,34 @@
             }
          }
       ?>
-      
+         <nav aria-label="Page navigation example">
+         <?php
+               if($bulunanSafyaSayisi>1) {?>
+                  <div class="paginationWrapper">
+                     <nav aria-label="Page navigation example ">
+                        <ul class="pagination">
+                        <li class="page-item"><a class="page-link" href="?sayfalama=1<? echo $sayfalamaKosulu?>">&laquo;</a></li>
+                        <?php
+                           for($i = $sayfalama-$sayfalamaIcinButonSayisi; $i <= $sayfalama+$sayfalamaIcinButonSayisi; $i++) {
+                              if(($i > 0) and ($i <= $bulunanSafyaSayisi)) {
+                                 $curr = $i;
+                              if($sayfalama == $i) {
+                                 echo "<li style=\"cursor: pointer\" class=\"page-item\"><div style=\"background: red; color: white\" class=\"page-link\">$curr</div></li>";
+                              } else {
+                                 echo "<li class=\"page-item\"><a class=\"page-link\" href=\"?sayfalama=$curr$sayfalamaKosulu\">$curr</a></li>";
+                              }
+                           }
+                        }
+                        ?>
+                           
+                           <li class="page-item"><a class="page-link"  href="?sayfalama=<?=$bulunanSafyaSayisi.$sayfalamaKosulu?>">&raquo;</a></li>
+                        </ul>
+                     </nav>
+                  </div>
+               <?php
+               }
+            ?>
+      </nav>
          <div class="mt-5 sendMess">
             <form method="post" action="./src/server/process.php">
                <input type="hidden" value="<?php echo $_GET["message"]?>" name="id">
